031e9638687c38ca18aab7ece8fef5e00227478b
euna99 <a653bc@naver.com>
2022-11-24 15:17:49 +0900

    1124

--- src/API.js
+++ src/API.js
@@ -1,52 +1,51 @@
 import axios from 'axios';
-import './App.css';
-import {useState, useEffect } from 'react';
-
-const URL = "/B551182/";
+import {useEffect,useState} from'react';
 function API() {
- 
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const fetchData = async () => {
-      try {
-        setError(null);
-        setData(null);
-        setLoading(true);
-  
-        const response = await axios.get(URL, {
-          params: {
-            serviceKey: process.env.REACT_APP_API_KEY,
-            numOfRows: 1,
-            pageNo: 10
-          }
-        });
-  
-        setData(response.data);
-      } catch(e) {
-        setError(e);
+
+const URL="https://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList"
+const [data, setData] = useState(null);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(null);
+const fetchData = async () => {
+  try {
+    setError(null);
+    setData(null);
+    setLoading(true);
+
+    const response=axios.get(URL,{
+      params:{
+        ServiceKey:process.env.REACT_APP_API_KEY,
+        pageN0:1,
+        numOfRows:10,
+        dataType:"XML",
+        dataCd:"ASOS",
+        dateCd:"DAY",
+        startDt:20100101,
+        endDt:20100601,
+        stnIds:108
       }
-      setLoading(false);
-    };
-  
-    useEffect(() => {
-      fetchData();
-    }, []);
-  
-    if(loading) return <div>Loading...</div>;
-    if(error)   return <div>Error...</div>;
-    if(!data)   return null;
-  
-    return (
-      <div className="App">
-        <p>병원명 : { data.response.body.items.item.yadmNm }</p>
-        <p>주소 : { data.response.body.items.item.addr }</p>
-        <p>전화번호 : { data.response.body.items.item.telno }</p>
-        <p>RAT(신속항원검사) 가능 여부 : { data.response.body.items.item.ratPsblYn }</p>
-        <p>PCR 가능 여부 : { data.response.body.items.item.pcrPsblYn }</p>
-      </div>
-    );
+    });
+
+    setData(response.data);
+  } catch(e) {
+    setError(e);
   }
- 
+  setLoading(false);
+};
+
+useEffect(() => {
+  fetchData();
+}, []);
+
+console.log (data)
+  if(loading) return <div>Loading...</div>;
+  if(error)   return <div>Error...</div>;
+  if(!data)   return null;
+
+  return(
+    <div>
+      {data.response.avgTa}
+    </div>
+  );
+  }
 export default API;

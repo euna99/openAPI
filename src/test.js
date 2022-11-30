9b7805c93006326edeb4416923c96052808ed4de
euna99 <a653bc@naver.com>
2022-11-30 21:57:47 +0900

    도서관 정보 받아오기 중

--- src/test.js
+++ src/test.js
@@ -1,33 +1,30 @@
-// import axios from "axios";
-// import { useEffect,useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-// function Test(){
-//     const API_KEY="RnKRYrieWKSXFg3fIq%2Fd%2BErYYCscM%2F1MAaDkN7iOvXc7r3vVny3HEe2ahsjoeGxTzUI3Cf71lPWfquc0GPykBg%3D%3D&";
-//     const [result, setResult] = useState({});
-//     const url="https://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList?serviceKey=${API_KEY}pageNo=1&numOfRows=10&spclAdmTyCd=A0"
+function Test(){
+    const [result, setResult] = useState({});
+    const API_KEY="RnKRYrieWKSXFg3fIq%2Fd%2BErYYCscM%2F1MAaDkN7iOvXc7r3vVny3HEe2ahsjoeGxTzUI3Cf71lPWfquc0GPykBg%3D%3D";
+    const url=`https://apis.data.go.kr/6460000/jnKolas/getSearchBookNewList?serviceKey=${API_KEY}&searchWord=%EC%84%9C%EC%9C%A0%EB%9F%BD&pageSize=10&startPage=0&`
+    const searchBook = async () => {
+        try {
+          const data= await axios({
+            method: 'get',
+            url: url,
+          })
+          setResult(data);
+          console.log("책"+data);
+        } 
+        catch(err) {
+          alert(err);
+        }
+    }
 
-//     const testget= async ()=>{
-//         try {
-//             const response = await axios({
-//               method: 'get',
-//               url: url,
-//             })
-//             setResult(response.data);
-//             console.log("test"+response.data
-            
-            
-            
-//             );
-//           } 
-//           catch(err) {
-//             alert(err);
-//           }
-//     }
+    useEffect(()=>{
+      searchBook();
+    },[])
 
-//     useEffect(()=>{
-//         testget();
-//         },[])
-// }
-
-
-// export default Test;
+    return (
+        <div></div>
+      );
+      }
+      export default Test;

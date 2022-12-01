51caef1e461cde2744d52d7758211fea72700d99
euna99 <a653bc@naver.com>
2022-12-01 21:23:44 +0900

    process.env 파일로 키 숨기기

--- src/test.js
+++ src/test.js
@@ -2,22 +2,20 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function Covid(){
-    const [data, setData] = useState(null);
-    const API_KEY="RnKRYrieWKSXFg3fIq%2Fd%2BErYYCscM%2F1MAaDkN7iOvXc7r3vVny3HEe2ahsjoeGxTzUI3Cf71lPWfquc0GPykBg%3D%3D";
-    const url=`/apis/1352000/ODMS_COVID_05/callCovid05Api?serviceKey=${API_KEY}&pageNo=1&numOfRows=500&apiType=json&create_dt=2022-01-08`
+    const [data, setData] = useState({});
+    const url=`/apis/1352000/ODMS_COVID_05/callCovid05Api?serviceKey=${process.env.REACT_APP_OPEN}&pageNo=1&numOfRows=500&apiType=json&create_dt=2022-01-08`
     
     const CovidCall = async () => {
         try {
           const response= await axios({
             method: 'get',
+            type:"json",
             url: url,
           });
-          setData(response.data)
-          console.log(response.data)
-          // setData(response.data);
-          // console.log("Covid"+response.data);
-          // console.log("---responsetype: "+typeof(response)); // 객체 //object
-          // console.log("---responsedatatype"+typeof(response.data)); //string // 이친구는 값이 나오고 
+          setData(response.data.body)
+          console.log("---COVID----"+response.data)
+          console.log("---responsetype: "+typeof(response)); // 객체 //object
+          console.log("---responsedatatype:"+typeof(response.data)); //string // 값이 나오고 
         } 
         catch(err) {
           alert(err);

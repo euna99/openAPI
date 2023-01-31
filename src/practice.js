3cfcd39a6a99f13fee632d4d340df9877829e57f
euna99 <a653bc@naver.com>
2023-01-31 17:01:48 +0900

    console 연습

---
+++ src/practice.js
@@ -0,0 +1,8 @@
+console.log ("practice");
+console.info("기능적을 로그와 같음");
+console.warn("경고");
+console.error("오류");
+
+let a = ['a','b','c','d']
+let map1=a.map(v=>v.toUpperCase());
+console.log(map1)

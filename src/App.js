2d0564e61ef93e8754631cfbcceddd75b0375bb4
euna99 <a653bc@naver.com>
2023-02-06 18:56:37 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './css/App.css';
 import Footer from './Footer';
 import Seoul from './seoul';
-import Covid from './test';
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-        {/* <Covid/> */}
-        <Seoul/>
-        <Footer/>
-    </div>
+
+<BrowserRouter>
+    <Routes>
+    {/* <Route path="/" /> */}
+    <Route path="/" element={<Seoul />}  />
+    </Routes>
+  <Footer/>
+</BrowserRouter> 
   );
 }
 
